e776aedd4271d6d4d59b69ad82b2fb7f2e0814a4
Stuart Marshall <stuart.marshall@flaxandteal.co.uk>
2026-08-05 16:13:09 +0100

    feat: write web_root tsconfig into the image

--- entrypoint.sh
+++ entrypoint.sh
@@ -263,19 +263,11 @@ run_npm_start() {
 	npm start
 }
 
-write_web_root_tsconfig() {
-	if [[ -f ${APP_FOLDER}/tsconfig.json ]]; then
-		echo "{ \"extends\": \"./${ARCHES_PROJECT}/tsconfig.json\" }" > ${WEB_ROOT}/tsconfig.json
-		echo "Wrote ${WEB_ROOT}/tsconfig.json -> ./${ARCHES_PROJECT}/tsconfig.json"
-	fi
-}
-
 run_npm_build_production() {
 	echo ""
 	echo ""
 	echo "----- RUNNING NPM BUILD PRODUCTION -----"
 	echo ""
-	write_web_root_tsconfig
 	cd_app_folder
 	npm run build_production
 }
@@ -285,7 +277,6 @@ run_npm_build_development() {
 	echo ""
 	echo "----- RUNNING NPM BUILD DEVELOPMENT -----"
 	echo ""
-	write_web_root_tsconfig
 	cd_app_folder
 	npm run build_development
 }
